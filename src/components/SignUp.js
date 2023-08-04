9d83d69b6b89ca0d2bfbfd8a27e4809d6cb4a9a3
leemiran0729 <leemiran.0729@gmail.com>
2023-08-04 20:29:04 +0900

    feat: 회원가입 페이지 UI 구성

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div>
+      <form>
+        <input type="text" data-testid="email-input" />
+        <input type="password" data-testid="password-input" />
+        <button type="submit" data-testid="signup-button">
+          회원가입
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
