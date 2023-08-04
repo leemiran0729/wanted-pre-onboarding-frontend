fc580611fd0768433161412744b72cb9e8baed2f
leemiran0729 <leemiran.0729@gmail.com>
2023-08-04 18:39:44 +0900

    feat: 필요한 컴포넌트 생성 및 라우터 정의

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn";
+import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/signup" />
-        <Route path="/signin" />
-        <Route path="/todo" />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/todo" element={<Todo />} />
       </Routes>
     </div>
   );
