61cda24cbdb7a70a187bdb646f49b065ccbcc004
leemiran0729 <leemiran.0729@gmail.com>
2023-08-05 17:13:08 +0900

    feat: Todo 페이지 리다이렉트 처리

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Todo = () => {
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (!localStorage.getItem("access_token")) {
+      navigate("/signin");
+    }
+  }, []);
+
   return <div>Todo</div>;
 };
 
